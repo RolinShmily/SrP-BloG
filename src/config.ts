128ab948c44968b6867ebdbc935c5645499fe29f
RolinShmily <rolinshmily@outlook.com>
2025-11-10 12:53:45 +0800

    fix(travelling):修改默认的跳转页面

--- src/config.ts
+++ src/config.ts
@@ -71,7 +71,7 @@ export const navBarConfig: NavBarConfig = {
     },
     {
       name: "开往",
-      url: "https://www.travellings.cn/go.html", // Internal links should not include the base path, as it is automatically added
+      url: "https://www.travellings.cn/train.html", // Internal links should not include the base path, as it is automatically added
       external: true, // Show an external link icon and will open in a new tab
     },
     {
