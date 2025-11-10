9789f7b41c6b8ce0e27b044b51c614635f89fdff
RolinShmily <rolinshmily@outlook.com>
2025-11-10 12:48:21 +0800

    feat(travelling):添加开往友链功能

--- src/config.ts
+++ src/config.ts
@@ -70,6 +70,11 @@ export const navBarConfig: NavBarConfig = {
       external: false, // Show an external link icon and will open in a new tab
     },
     {
+      name: "开往",
+      url: "https://www.travellings.cn/go.html", // Internal links should not include the base path, as it is automatically added
+      external: true, // Show an external link icon and will open in a new tab
+    },
+    {
       name: "名片",
       url: "https://link.srprolin.top", // Internal links should not include the base path, as it is automatically added
       external: true, // Show an external link icon and will open in a new tab
