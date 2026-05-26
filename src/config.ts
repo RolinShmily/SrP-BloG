ea29362f0db5429053650760b52bcb20b421589b
RoL1n <RolinShmily@outlook.com>
2026-05-26 22:51:27 +0800

    fix: 更新导航栏名片链接地址

--- src/config.ts
+++ src/config.ts
@@ -105,7 +105,7 @@ export const navBarConfig: NavBarConfig = {
 		},
 		{
 			name: "名片",
-			url: "https://link.srprolin.top", // Internal links should not include the base path, as it is automatically added
+			url: "https://www.srprolin.top", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
